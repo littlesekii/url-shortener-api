e63f1bef6d000e2d600eef9a702d5a861884c448
littlesekii <contact.sekii@gmail.com>
2025-04-29 12:52:25 -0300

    patch.: removed unused imports

--- src/main/java/cat/linky/urlshortener_api/core/controller/PingController.java
+++ src/main/java/cat/linky/urlshortener_api/core/controller/PingController.java
@@ -1,10 +1,8 @@
 package cat.linky.urlshortener_api.core.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 
