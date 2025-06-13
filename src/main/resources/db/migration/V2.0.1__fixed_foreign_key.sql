19c0e755f5b73422cc8fc6aa5344f25c0a29aaa6
littlesekii <contact.sekii@gmail.com>
2025-06-13 17:38:32 -0300

    patch.: fixed foreign key data type

--- src/main/resources/db/migration/V2.0.1__fixed_foreign_key.sql
+++ src/main/resources/db/migration/V2.0.1__fixed_foreign_key.sql
@@ -1,15 +1,1 @@
-create table t_short_url (
-    id bigserial primary key,
-    short_hash varchar(5) not null,
-    short_url varchar(255) not null,
-    target_url varchar not null
-);
-
-create table t_short_url_interaction (
-    id bigserial primary key,
-    id_short_url bigint not null references t_short_url(id),
-    moment timestamp not null
-);
-
-drop table t_urlref;
-drop table t_urlref_interaction;
+ALTER TABLE t_short_url_interaction MODIFY COLUMN id_short_url bigint;
